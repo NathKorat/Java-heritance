3f12907c100eacdeebeac25b45bef2a190700c29
NathKorat <nathkoratpsy@gmail.com>
2022-12-15 19:57:40 +0700

    add new function

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
-import staticPack.*;
 import animalPack.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Dog dog1 = new Dog();
+        dog1.bark();
+        dog1.eat();
+        dog1.run();
         
     }
 }
