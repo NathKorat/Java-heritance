95b26f4289a941bdafae0e729572879d5009a4a1
NathKorat <nathkoratpsy@gmail.com>
2022-12-15 20:07:25 +0700

    modified cat obj

--- src/App.java
+++ src/App.java
@@ -3,9 +3,11 @@ import animalPack.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Dog dog1 = new Dog();
+        Cat cat1 = new Cat();
         dog1.bark();
         dog1.eat();
         dog1.run();
         
+        cat1.eat();
     }
 }
