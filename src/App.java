f85e8c42f7418f52da51a72c9d9b446cc4365f21
NathKorat <nathkoratpsy@gmail.com>
2022-12-15 19:53:15 +0700

    fix bugs

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import staticPack.*;
 import animalPack.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Dog dog1 = new Dog();
-        
+        dog1.bark();
+
     }
 }
